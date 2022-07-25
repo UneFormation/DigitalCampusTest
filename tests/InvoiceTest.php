beed20e29c77eec4b4f1fb6f69b1b5f1c523c36b
David Patiashvili <stratagem.david@gmail.com>
2022-07-25 14:12:55 +0200

    Add first unittest

---
+++ tests/InvoiceTest.php
@@ -0,0 +1,16 @@
+<?php
+
+use \PHPUnit\Framework\TestCase;
+
+class InvoiceTest extends TestCase
+{
+    public function toto()
+    {
+        echo 'TOTOT';
+    }
+
+    public function testTrue()
+    {
+        $this->assertTrue(true);
+    }
+}
